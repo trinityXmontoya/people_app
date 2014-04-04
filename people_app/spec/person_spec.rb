5707ccea5cf27428df2516db940e26b7e5a859ea
trinityXmontoya <trinityXmontoya@gmail.com>
2014-04-04 17:40:29 -0400

    name test passes

--- people_app/spec/person_spec.rb
+++ people_app/spec/person_spec.rb
@@ -1,2 +1,12 @@
-require 'spec_helper'
-require_relative '../lib/person'
+require_relative '../app/models/person'
+
+  describe Person do
+
+  before(:each){@elon=Person.new("Elon","Musk",1971-06-28,2,'https://pbs.twimg.com/profile_images/420314816444502016/xj5TnUsx.jpeg',true)}
+
+    it "has a full name" do
+     expect(@elon.name).to eq('Elon Musk')
+    end#end full name method
+
+
+end#end person
