a3f32fe53b41b5a989a6747295cd36b469449669
trinityXmontoya <trinityXmontoya@gmail.com>
2014-04-04 18:33:18 -0400

    age test passes

--- people_app/spec/person_spec.rb
+++ people_app/spec/person_spec.rb
@@ -2,11 +2,18 @@ require_relative '../app/models/person'
 
   describe Person do
 
-  before(:each){@elon=Person.new("Elon","Musk",1971-06-28,2,'https://pbs.twimg.com/profile_images/420314816444502016/xj5TnUsx.jpeg',true)}
+  before(:each){@elon=Person.new("Elon","Musk",'1971-06-28',2,'https://pbs.twimg.com/profile_images/420314816444502016/xj5TnUsx.jpeg',true)}
 
     it "has a full name" do
      expect(@elon.name).to eq('Elon Musk')
     end#end full name method
 
+    # it 'has a birthday' do
+    #   expect(@elon.birthdate).to eq(1971-06-28)
+    # end
 
-end#end person
+    it 'has an age' do
+      expect(@elon.age).to eq(43)
+    end
+
+  end#end person
