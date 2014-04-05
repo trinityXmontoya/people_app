934e56bda598a5bb805b4b8e9c54abfce299946b
trinityXmontoya <trinityXmontoya@gmail.com>
2014-04-05 18:24:56 -0400

    update spec file

--- people_app/spec/person_spec.rb
+++ people_app/spec/person_spec.rb
@@ -2,81 +2,81 @@ require_relative '../app/models/person'
 
   describe Person do
 
-  before(:each){@elon=Person.new("Elon","Musk",'1971-06-28',2,'https://pbs.twimg.com/profile_images/420314816444502016/xj5TnUsx.jpeg',true)}
+    subject(:elon){Person.create(
+                  first_name: "Elon",
+                  last_name:"Musk",
+                  birthdate:'1971-06-28',
+                  img_url:'https://pbs.twimg.com/profile_images/420314816444502016/xj5TnUsx.jpeg',
+                  license: true)}
+    subject(:lorde){Person.create("Ella","Yelich-O;Connor",'1996-09-28',0,'http://bit.ly/ti8S6KF',false)}
+    subject(:norm){Person.create("Norm","Peterson",'1948-10-17',3,'http://2.bp.blogspot.com/-qv0NdZKA2_Y/UZPqouH9gWI/AAAAAAAAHYc/NC0BCjgzEzU/s320/1-norm-cheers.jpg',true)}
+    subject(:yashar){Person.create("Yashar","M",'1997-02-28',0,'yashar.jpeg',false)}
+    subject(:trinity){Person.create("Trinity","M",'1993-07-02',0,'trinity.jpeg',false)}
 
   describe '.name' do
-    it "returns a full name" do
-     expect(@elon.name).to eq('Elon Musk')
+    it "returns a string that is the person's full name" do
+     expect(elon.name).to eq('Elon Musk')
     end#end full name method
   end#.name
 
   describe '.birthday' do
-    it 'returns birthdate as a string' do
-      expect(@elon.birthday).to eq('1971-06-28')
+    it 'returns the person\'s birthdate as a string' do
+      expect(elon.birthday).to eq('1971-06-28')
     end
   end#.birthday
 
   describe '.age' do
-    it 'has an age' do
-      expect(@elon.age).to eq(42)
+    it 'returns the person\'s age' do
+      expect(elon.age).to eq(42)
     end
   end#.age
 
   describe '.have_a_drink' do
 
-    it 'can only drink if over 21' do
-      @lorde=Person.new("Ella","Yelich-O;Connor",'1996-09-28',0,'http://bit.ly/ti8S6KF',false)
-      expect(@elon.have_a_drink).to eq(true)
-      expect(@lorde.have_a_drink).to eq("Wait a few years")
+    it 'allows the person to drink if over 21' do
+      expect(elon.have_a_drink).to eq(true)
+      expect(lorde.have_a_drink).to eq("Wait a few years")
     end
 
-    it 'cannot have more than 3 drinks' do
-      @elon.have_a_drink
-      expect(@elon.have_a_drink).to eq("Go home you're drunk")
+    it 'only allows the person to have 3 drinks max' do
+      elon.have_a_drink
+      expect(elon.have_a_drink).to eq("Go home you're drunk")
     end
   end#.have_a_drink
 
+  describe '.drunk' do
+    it 'returns true if the person has had 3 drinks' do
+
+      expect(norm.drunk?).to eq(true)
+
+    end
+  end
+
     describe '.drive_a_car' do
 
-      it 'does not allow a person without a license to drive' do
-        @trinity=Person.new("Trinity","M",'1993-07-02',0,'jpg.jpeg',false)
-
-        expect(@trinity.age).to eq(20)
-        expect(@trinity.drive_a_car).to eq(false)
+      it 'only allows a person with a license to drive' do
+        expect(trinity.age).to eq(20)
+        expect(trinity.drive_a_car).to eq(false)
       end
 
       it 'does not allow a person under 18 to drive' do
-        @rose=Person.new("Rose","T",'1999-09-12',0,'jpeg.jpeg',false)
-
-        expect(@rose.age).to eq(15)
-        expect(@rose.drive_a_car).to eq("Not yet youngin")
+        expect(rose.age).to eq(15)
+        expect(rose.drive_a_car).to eq("Not yet youngin")
       end#under 18
 
       it 'does not allow a drunk person to drive' do
-        @norm=Person.new("Norm","Peterson",'1948-10-17',3,'http://2.bp.blogspot.com/-qv0NdZKA2_Y/UZPqouH9gWI/AAAAAAAAHYc/NC0BCjgzEzU/s320/1-norm-cheers.jpg',true)
 
-        expect(@norm.age).to eq(66)
-        expect(@norm.drunk?).to eq(true)
-        expect(@norm.drive_a_car).to eq("Looks like a cab for you tonight")
+        expect(norm.age).to eq(66)
+        expect(norm.drunk?).to eq(true)
+        expect(norm.drive_a_car).to eq("Looks like a cab for you tonight")
       end
-
-      it 'allows a sober person 18 and over with a license to drive' do
-        @adrianna=Person.new("Adrianna","U",'1993-02-22',2,'http://bit.ly/ti8S6KF',true)
-
-        expect(@adrianna.age).to eq(21)
-        expect(@adrianna.drunk?).to eq(false)
-        expect(@adrianna.drive_a_car).to eq(true)
-      end
-
-
     end#.drive_a_car
 
     describe ".sober_up" do
-      it 'reduces the number of drinks a person has' do
-        @norm=Person.new("Norm","Peterson",'1948-10-17',3,'http://2.bp.blogspot.com/-qv0NdZKA2_Y/UZPqouH9gWI/AAAAAAAAHYc/NC0BCjgzEzU/s320/1-norm-cheers.jpg',true)
+      it 'reduces the number of drinks a person has by one' do
 
-        @norm.sober_up
-        expect(@norm.num_of_drinks).to eq(2)
+        norm.sober_up
+        expect(norm.num_of_drinks).to eq(2)
       end
     end
 
