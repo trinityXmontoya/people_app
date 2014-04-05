276c95a0d710436a7ecf7482b36ec57949958bba
trinityXmontoya <trinityXmontoya@gmail.com>
2014-04-04 22:48:09 -0400

    sober up test pass

--- people_app/app/models/person.rb
+++ people_app/app/models/person.rb
@@ -1,2 +1,44 @@
-class Person < ActiveRecord::Base
+require 'date'
+
+class Person < Struct.new(:first_name,:last_name,:birthdate,:num_of_drinks,:img_url,:license)
+
+  def name
+    return "#{first_name} #{last_name}"
+  end
+
+  def birthday
+    birthdate.to_s
+  end
+
+  def age
+    now = Time.now.utc.to_date
+    birthday=Date.parse(birthday)
+    (now.year - birthday.year) - (birthday.to_date.change(:year => now.year) > now ? 1 : 0)
+  end
+
+  def have_a_drink
+    if num_of_drinks < 3
+      (age > 21) ? (self.num_of_drinks += 1 and true) : "Wait a few years"
+    else
+        return "Go home you're drunk"
+    end
+  end
+
+  def drunk?
+    num_of_drinks >= 3
+  end
+
+  def drive_a_car
+    case
+      when age < 18 then "Not yet youngin"
+      when age > 21 && (license && drunk?) then "Looks like a cab for you tonight"
+      when age >= 18 && license then true
+    end
+  end
+
+  def sober_up
+    if num_of_drinks!=0 then self.num_of_drinks-=1 end
+  end
+
 end
+
--- people_app/spec/person_spec.rb
+++ people_app/spec/person_spec.rb
@@ -4,26 +4,81 @@ require_relative '../app/models/person'
 
   before(:each){@elon=Person.new("Elon","Musk",'1971-06-28',2,'https://pbs.twimg.com/profile_images/420314816444502016/xj5TnUsx.jpeg',true)}
 
-    it "has a full name" do
+  describe '.name' do
+    it "returns a full name" do
      expect(@elon.name).to eq('Elon Musk')
     end#end full name method
+  end#.name
 
-    # it 'has a birthday' do
-    #   expect(@elon.birthdate).to eq(1971-06-28)
-    # end
-
-    it 'has an age' do
-      expect(@elon.age).to eq(43)
+  describe '.birthday' do
+    it 'returns birthdate as a string' do
+      expect(@elon.birthday).to eq('1971-06-28')
     end
+  end#.birthday
 
-    it 'can drink if over 21' do
+  describe '.age' do
+    it 'has an age' do
+      expect(@elon.age).to eq(42)
+    end
+  end#.age
+
+  describe '.have_a_drink' do
+
+    it 'can only drink if over 21' do
+      @lorde=Person.new("Ella","Yelich-O;Connor",'1996-09-28',0,'http://bit.ly/ti8S6KF',false)
       expect(@elon.have_a_drink).to eq(true)
-      expect(@elon.num_of_drinks).to eq(3)
+      expect(@lorde.have_a_drink).to eq("Wait a few years")
     end
 
     it 'cannot have more than 3 drinks' do
       @elon.have_a_drink
       expect(@elon.have_a_drink).to eq("Go home you're drunk")
     end
+  end#.have_a_drink
+
+    describe '.drive_a_car' do
+
+      it 'does not allow a person without a license to drive' do
+        @trinity=Person.new("Trinity","M",'1993-07-02',0,'jpg.jpeg',false)
+
+        expect(@trinity.age).to eq(20)
+        expect(@trinity.drive_a_car).to eq(false)
+      end
+
+      it 'does not allow a person under 18 to drive' do
+        @rose=Person.new("Rose","T",'1999-09-12',0,'jpeg.jpeg',false)
+
+        expect(@rose.age).to eq(15)
+        expect(@rose.drive_a_car).to eq("Not yet youngin")
+      end#under 18
+
+      it 'does not allow a drunk person to drive' do
+        @norm=Person.new("Norm","Peterson",'1948-10-17',3,'http://2.bp.blogspot.com/-qv0NdZKA2_Y/UZPqouH9gWI/AAAAAAAAHYc/NC0BCjgzEzU/s320/1-norm-cheers.jpg',true)
+
+        expect(@norm.age).to eq(66)
+        expect(@norm.drunk?).to eq(true)
+        expect(@norm.drive_a_car).to eq("Looks like a cab for you tonight")
+      end
+
+      it 'allows a sober person 18 and over with a license to drive' do
+        @adrianna=Person.new("Adrianna","U",'1993-02-22',2,'http://bit.ly/ti8S6KF',true)
+
+        expect(@adrianna.age).to eq(21)
+        expect(@adrianna.drunk?).to eq(false)
+        expect(@adrianna.drive_a_car).to eq(true)
+      end
+
+
+    end#.drive_a_car
+
+    describe ".sober_up" do
+      it 'reduces the number of drinks a person has' do
+        @norm=Person.new("Norm","Peterson",'1948-10-17',3,'http://2.bp.blogspot.com/-qv0NdZKA2_Y/UZPqouH9gWI/AAAAAAAAHYc/NC0BCjgzEzU/s320/1-norm-cheers.jpg',true)
+
+        @norm.sober_up
+        expect(@norm.num_of_drinks).to eq(2)
+      end
+    end
+
 
   end#end person
